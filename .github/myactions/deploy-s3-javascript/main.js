3e8f3c773ce7b2eaf5f4e986996cdf40b8e316ab
joergbeicht <joergbeicht@confessio-management.de>
2023-11-11 15:23:04 +0100

    added output

--- .github/myactions/deploy-s3-javascript/main.js
+++ .github/myactions/deploy-s3-javascript/main.js
@@ -17,9 +17,11 @@ function run() {
   // Angabe localFolder (also der distFolder) zu s3Folder (hier s3Uri)
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
-  core.notice('Hello from my custom JavaScript Action 2!');
-  // const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
-  // core.setOutput('website-url', websiteUrl); // ::set-output
+  // core.notice('Hello from my custom JavaScript Action 2!');
+
+  // http://gha-custom-action-hosting-jb.s3-website-us-east-1.amazonaws.com/
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput('website-url', websiteUrl); // ::set-output
 }
 
 run();
