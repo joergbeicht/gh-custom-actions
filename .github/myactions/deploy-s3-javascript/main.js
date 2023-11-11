2121f4dc351f7c8d9ac4df6d638beacadd806816
joergbeicht <joergbeicht@confessio-management.de>
2023-11-11 15:14:55 +0100

    added custom deploy action

--- .github/myactions/deploy-s3-javascript/main.js
+++ .github/myactions/deploy-s3-javascript/main.js
@@ -3,16 +3,21 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 
 function run() {
-  core.notice('Hello from my custom JavaScript Action!');
   // 1) Get some input values
-  // const bucket = core.getInput('bucket', { required: true });
-  // const bucketRegion = core.getInput('bucket-region', { required: true });
-  // const distFolder = core.getInput('dist-folder', { required: true });
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
 
-  // // 2) Upload files
-  // const s3Uri = `s3://${bucket}`;
-  // exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+  // github.getOctokit().auth => Zugriff auf REST-API
+  // github.context.action => Zugriff auf Workflow
 
+  // 2) Upload files
+  const s3Uri = `s3://${bucket}`;
+  
+  // Angabe localFolder (also der distFolder) zu s3Folder (hier s3Uri)
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+  core.notice('Hello from my custom JavaScript Action 2!');
   // const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
   // core.setOutput('website-url', websiteUrl); // ::set-output
 }
